f3cd82d97250481ca05beadfe6773d93270f60ca
amItprimo27 <amitprimo27@gmail.com>
2026-01-12 19:42:00 +0200

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.error(error);
+});
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//TODO: Add your routes here
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
